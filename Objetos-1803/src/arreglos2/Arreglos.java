a8a447588e3ea5e21b3e90fe333737a61af98419
Gus <Gus@Gus-PC.gateway.2wire.net>
2018-06-24 21:16:19 -0500

    Arreglos no mutables

---
+++ Objetos-1803/src/arreglos2/Arreglos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos2;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Gus
+ */
+public class Arreglos {
+   
+    
+    public static void main(String[] args) {
+     
+    String Hola[]={"Hola","MUNDO"} ;
+    System.out.println(Hola.length);
+     
+    int[] x = new int[3];
+    x[0]=10;
+    x[1]=5;
+    x[2]=15;
+    
+        System.out.println(x.length);
+    }
+}
